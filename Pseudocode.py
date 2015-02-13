110f4bcc267fcc10ace7406354b01c05f13c7c2f
Sarah Tolley <stolley@ucla.edu>
2015-02-13 19:13:09 +0000

    Pseudocode for final project

---
+++ Pseudocode.py
@@ -0,0 +1,36 @@
+#pseudocode for final project
+#Find the sampling distribution of Wolbachia from ants sampled in Russell, 2012 (Myrmecological News)
+
+
+#ant_species = list of ant species surveyed in Russell 2012
+
+
+#extract genus names from ant_species to identify genera that dominate sampling efforts that have been done and to highlight genera that need more attention
+#count number of each genus present 
+#return value for each genus 
+
+
+#Identify each species' infection status and group uninfected and infected species together
+#def infection_status(ant_species):
+	#positive = any line with a "+"
+	#negative = any line with a "-"
+
+
+#Compare the percent of infected and uninfected species in each genus to determine if there are any patterns in infection
+#for "genus" in ant_species:
+	#return % where infection_status = positive for each genus
+	#return % where infection_status = negative for each genus
+
+#If data is available, look at life histories of infected vs. noninfected species 
+#for "life_history" in ant_species:
+	#if "+", return life history characteristics 
+	#if "-", return life history characteristics 
+
+#Look at geographic distribution of Wolbachia infections 
+#for "collection_site" in ant_species:
+	#return list of sites where Wolbachia was found in ants
+	#return list of sites where Wolbachia was not found in sampled species
+
+#Make relevant figures to visualize the results of each test
+
+
